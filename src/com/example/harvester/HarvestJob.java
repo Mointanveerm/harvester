3c4495194c24cf5c723e14d9d7f861bddfb92015
Mointanveerm <mointanveer1008@gmail.com>
2026-08-19 20:47:21 +0500

    Fix HarvestJob compilation

--- src/com/example/harvester/HarvestJob.java
+++ src/com/example/harvester/HarvestJob.java
@@ -4,6 +4,33 @@ import android.app.job.JobParameters;
 import android.app.job.JobService;
 
 public class HarvestJob extends JobService {
+    @Override
+    public boolean onStartJob(final JobParameters p) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    HarvestEngine.run(getApplicationContext());
+                } catch (Throwable t) {
+                    /* fail silently */
+                }
+                jobFinished(p, false);
+            }
+        }).start();
+
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters p) {
+        return true;  // retry next cycle if killed
+    }
+}package com.example.harvester;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+
+public class HarvestJob extends JobService {
     @Override public boolean onStartJob(JobParameters p) {
         new Thread(() -> {
             try {
