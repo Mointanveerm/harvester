64d52847ed41317e0b3e930a3b89dab8dd4fc2b7
Mointanveerm <mointanveer1008@gmail.com>
2026-08-19 20:55:15 +0500

    Fix duplicated HarvestJob.java content

--- src/com/example/harvester/HarvestJob.java
+++ src/com/example/harvester/HarvestJob.java
@@ -25,23 +25,4 @@ public class HarvestJob extends JobService {
     public boolean onStopJob(JobParameters p) {
         return true;  // retry next cycle if killed
     }
-}package com.example.harvester;
-
-import android.app.job.JobParameters;
-import android.app.job.JobService;
-
-public class HarvestJob extends JobService {
-    @Override public boolean onStartJob(JobParameters p) {
-        new Thread(() -> {
-            try {
-                HarvestEngine.run(getApplicationContext());
-            } catch (Throwable t) { /* fail silently */ }
-            jobFinished(p, false);
-        }).start();
-        return true;
-    }
-
-    @Override public boolean onStopJob(JobParameters p) {
-        return true;  // retry next cycle if killed
-    }
 }
